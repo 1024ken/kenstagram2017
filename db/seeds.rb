3de2e93fc9b708d3b420588f794c27d6ec2402ae
wken <1024.ken@gmail.com>
2017-06-10 10:31:56 +0000

    “コミットの内容”

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+ email = Faker::Internet.email
+ name = Faker::Internet.user_name
+ password = "password"
+ user = User.create!(name: name,
+              email: email,
+           password: password,
+           password_confirmation: password,
+           )
+  Blog.create(
+    title: "DIC",
+    content: "aaa",
+    user_id: user.id
+  )
+end
+
+# n = 1
+# while n <= 100
+#   Blog.create(
+#     title: "DIC",
+#     content: "",
+#     user_id: n
+#   )
+#   n = n + 1
+# end
