2cc53a08306cea35c6f6a0a7e97d1ceafa57d9d7
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2017-06-24 17:45:13 +0900

    sample commit

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCalController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
     if @user.persisted?
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
@@ -9,11 +8,10 @@ class Users::OmniauthCalController < Devise::OmniauthCallbacksController
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+  end
 
-    def twitter
-
+  def twitter
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
-
     if @user.persisted?
       set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
