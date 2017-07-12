b12ce16fd2eaf1ba5b278e83325822826d6d14e9
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2017-07-12 16:21:17 +0900

    make it kenstagram

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170701085828) do
     t.string   "image"
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
